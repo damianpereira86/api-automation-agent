3b7c64c021b71ccb1c7606df06bcca2960138a72
Emma <jmbh076@gmail.com>
2024-11-19 10:18:42 -0300

    Improve logging

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,4 +1,6 @@
 import logging
+import sys
+from typing import Optional
 
 from src.configuration.config import Config
 
@@ -6,8 +8,27 @@ from src.configuration.config import Config
 class Logger:
     @staticmethod
     def configure_logger(config: Config):
+        log_level = logging.DEBUG if config.debug else logging.INFO
+
+        # Default formats
+        stdout_format = "%(message)s"
+        file_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
+        # Handlers
+        stdout_handler = logging.StreamHandler(sys.stdout)
+        stdout_handler.setLevel(log_level)
+        stdout_handler.setFormatter(logging.Formatter(stdout_format))
+
+        file_handler = logging.FileHandler(
+            "logs/" + config.destination_folder.split("/")[-1] + ".log"
+        )
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(logging.Formatter(file_format))
+
         logging.basicConfig(
-            format="%(message)s", level=logging.DEBUG if config.debug else logging.INFO
+            format="%(message)s",
+            level=log_level,
+            handlers=[stdout_handler, file_handler],
         )
         logging.getLogger("httpx").setLevel(logging.WARNING)
 
